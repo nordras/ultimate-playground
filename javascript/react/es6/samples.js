1d875ff38109d208cfecc10ed33d7e086b6ba8be
Igor <igorkmail@gmail.com>
2026-01-17 00:41:39 -0300

    Call all es6 samples on one file

---
+++ javascript/react/es6/samples.js
@@ -0,0 +1,87 @@
+// ============================================
+// ES6 (ECMAScript 2015) - Principais Features
+// ============================================
+
+import { demonLetConst } from './01-let-const.js';
+import { demonstrateArrowFunctions } from './02-arrow-functions.js';
+import { demonstrateTemplateLiterals } from './03-template-literals.js';
+import { demonstrateDestructuring } from './04-destructuring.js';
+import { demonstrateSpreadOperator } from './05-spread-operator.js';
+import { demonstrateRestParameters } from './06-rest-parameters.js';
+import { demonstrateDefaultParameters } from './07-default-parameters.js';
+import { demonstrateClasses } from './08-classes.js';
+import { demonstratePromises } from './09-promises.js';
+import { demonstrateEnhancedObjectLiterals } from './10-enhanced-object-literals.js';
+import { demonstrateArrayMethods } from './11-array-methods.js';
+import { demonstrateForOfLoop } from './12-for-of-loop.js';
+import { demonstrateMap } from './13-map.js';
+import { demonstrateSet } from './14-set.js';
+import { demonstrateSymbol } from './15-symbol.js';
+import { generators } from './16-generators.js';
+import { demonstrateModules } from './17-modules.js';
+import { demonstrateObjectAssign } from './18-object-assign.js';
+import { StringMethods } from './19-string-methods.js';
+import { ArrayFromOf } from './20-array-from-of.js';
+
+console.log('=== ES6 FEATURES ===\n');
+
+demonLetConst();
+console.log('\n');
+
+demonstrateArrowFunctions();
+console.log('\n');
+
+demonstrateTemplateLiterals();
+console.log('\n');
+
+demonstrateDestructuring();
+console.log('\n');
+
+demonstrateSpreadOperator();
+console.log('\n');
+
+demonstrateRestParameters();
+console.log('\n');
+
+demonstrateDefaultParameters();
+console.log('\n');
+
+demonstrateClasses();
+console.log('\n');
+
+demonstratePromises();
+console.log('\n');
+
+demonstrateEnhancedObjectLiterals();
+console.log('\n');
+
+demonstrateArrayMethods();
+console.log('\n');
+
+demonstrateForOfLoop();
+console.log('\n');
+
+demonstrateMap();
+console.log('\n');
+
+demonstrateSet();
+console.log('\n');
+
+demonstrateSymbol();
+console.log('\n');
+
+generators();
+console.log('\n');
+
+demonstrateModules();
+console.log('\n');
+
+demonstrateObjectAssign();
+console.log('\n');
+
+StringMethods();
+console.log('\n');
+
+ArrayFromOf();
+console.log('\n');
+
