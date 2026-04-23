ea2b9bc6260b684fd6f518c81f6f57bb47fd3db8
Igor <igorkmail@gmail.com>
2026-04-23 14:08:53 -0300

    p35

---
+++ javascript/99-problems/p35/main.js
@@ -0,0 +1,38 @@
+function primeFactors(number) {
+  const factors = [];
+  let remainder = number;
+  let divisor = 2;
+
+  while (remainder > 1) {
+    while (remainder % divisor === 0) {
+      factors.push(divisor);
+      remainder /= divisor;
+    }
+
+    divisor += divisor === 2 ? 1 : 2;
+
+    if (divisor * divisor > remainder && remainder > 1) {
+      factors.push(remainder);
+      break;
+    }
+  }
+
+  return factors;
+}
+
+function primeFactorsMult(number) {
+  if (!Number.isInteger(number) || number < 2) {
+    throw new Error('number must be an integer greater than 1');
+  }
+
+  const grouped = primeFactors(number).reduce((counts, factor) => {
+    counts[factor] = (counts[factor] || 0) + 1;
+    return counts;
+  }, {});
+
+  return Object.entries(grouped)
+    .map(([factor, multiplicity]) => [Number(factor), multiplicity])
+    .sort((first, second) => first[0] - second[0]);
+}
+
+module.exports = primeFactorsMult;
