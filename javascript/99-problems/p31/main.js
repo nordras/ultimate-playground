c527c69027965db5777e74cbea2c7f8071c7da9c
Igor <igorkmail@gmail.com>
2026-04-23 18:26:54 -0300

    add title

--- javascript/99-problems/p31/main.js
+++ javascript/99-problems/p31/main.js
@@ -1,3 +1,4 @@
+// Determine the greatest common divisor of two positive integer numbers.
 function gcd(first, second) {
   if (!Number.isInteger(first) || !Number.isInteger(second)) {
     throw new Error('Both numbers must be integers');
