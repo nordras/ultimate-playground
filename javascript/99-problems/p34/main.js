627b9544c2d1cde17b825a69239c42cd5263a5b4
Igor <igorkmail@gmail.com>
2026-04-23 14:08:52 -0300

    p34

---
+++ javascript/99-problems/p34/main.js
@@ -0,0 +1,27 @@
+function primeFactors(number) {
+  if (!Number.isInteger(number) || number < 2) {
+    throw new Error('number must be an integer greater than 1');
+  }
+
+  const factors = [];
+  let remainder = number;
+  let divisor = 2;
+
+  while (remainder > 1) {
+    while (remainder % divisor === 0) {
+      factors.push(divisor);
+      remainder /= divisor;
+    }
+
+    divisor += divisor === 2 ? 1 : 2;
+
+    if (divisor * divisor > remainder && remainder > 1) {
+      factors.push(remainder);
+      break;
+    }
+  }
+
+  return factors;
+}
+
+module.exports = primeFactors;
