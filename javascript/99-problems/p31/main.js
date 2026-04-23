78403c9be056319d4d0ec6acb592cbdebed91fda
Igor <igorkmail@gmail.com>
2026-04-23 14:08:52 -0300

    p31

---
+++ javascript/99-problems/p31/main.js
@@ -0,0 +1,18 @@
+function gcd(first, second) {
+  if (!Number.isInteger(first) || !Number.isInteger(second)) {
+    throw new Error('Both numbers must be integers');
+  }
+
+  let a = Math.abs(first);
+  let b = Math.abs(second);
+
+  while (b !== 0) {
+    const remainder = a % b;
+    a = b;
+    b = remainder;
+  }
+
+  return a;
+}
+
+module.exports = gcd;
