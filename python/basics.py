42ed4db838689e32e17b0e9ea1f2b14bdb39d27e
Igor <igorkmail@gmail.com>
2026-05-20 23:42:59 -0300

    strings

--- python/basics.py
+++ python/basics.py
@@ -18,3 +18,31 @@ print(type(text))      # <class 'str'>
 print(type(boolean))   # <class 'bool'>
 print(type(null))      # <class 'NoneType'>
 
+
+# ------------------------------------------------------------
+# 2. Strings
+# ------------------------------------------------------------
+
+first_name = "Mary"
+last_name = 'Smith'
+
+# Concatenation
+full_name = first_name + " " + last_name
+print(full_name)  # Mary Smith
+
+# f-string (modern formatting)
+age = 30
+print(f"{first_name} is {age} years old.")
+
+# String methods
+print(text.upper())                     # HELLO, PYTHON!
+print(text.lower())                     # hello, python!
+print(text.replace("Python", "World"))  # Hello, World!
+print(len(text))                        # string length
+
+# Slicing
+word = "Python"
+print(word[0])      # P
+print(word[-1])     # n
+print(word[0:3])    # Pyt
+print(word[::-1])   # nohtyP (reversed)
