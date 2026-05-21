ad0d3a8731b060f561bf5bf3ee1b88708e37aa4c
Igor <igorkmail@gmail.com>
2026-05-20 23:42:25 -0300

    python init

---
+++ python/basics.py
@@ -0,0 +1,20 @@
+# ============================================================
+# Syntax and Core Concepts
+# ============================================================
+
+# ------------------------------------------------------------
+# 1. Variables and Data Types
+# ------------------------------------------------------------
+
+integer = 42
+floating = 3.14
+text = "Hello, Python!"
+boolean = True
+null = None
+
+print(type(integer))   # <class 'int'>
+print(type(floating))  # <class 'float'>
+print(type(text))      # <class 'str'>
+print(type(boolean))   # <class 'bool'>
+print(type(null))      # <class 'NoneType'>
+
